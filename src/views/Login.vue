9a053224ad0f99b1b4a9d32bdcb3ba3a1346ee7a
SpaceLenore <libreterra@protonmail.com>
2019-11-03 20:40:07 +0100

    WIP: connect page to api

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -5,17 +5,20 @@
                 <div class="half-card dark">
                     <h1>Login</h1>
                     <p>
-                        By logging in you get access to all your online wallets and trading options. Shitty economical misstakes right at your
-                        fingertips accessible from anywhere in the world!
+                        By logging in you get access to all your online wallets and trading options.
+                        Shitty economical misstakes right at your fingertips accessible from anywhere in the world!
                     </p>
                 </div>
                 <div class="half-card light">
                     <div class="login-form">
                         <form @submit.prevent="sendForm">
-                            <input type="email" placeholder="Email">
-                            <input type="password" placeholder="Password">
+                            <input type="email" placeholder="Email" v-model="email">
+                            <input type="password" placeholder="Password" v-model="password">
                             <input type="submit" value="Login">
                         </form>
+                    </div>
+                    <div v-if="error" class="error">
+                        {{error}}
                     </div>
                 </div>
             </div>
@@ -28,17 +31,27 @@ import Api from "@/Apicaller.js";
 
 export default {
     name: "login",
-    components: {
-        Navbar
-    },
     data() {
         return {
-
+            email: null,
+            password: null,
+            error: null
         }
     },
     methods: {
-        sendForm: function() {
+        async sendForm() {
+            if (!this.email || !this.password) {
+                this.error = "All fields must be filled out";
+                return false;
+            }
 
+            this.error = null;
+            let response = await Api.req("/login" , 'POST', false, {
+                email: this.email,
+                password: this.password
+            });
+
+            console.log(response);
         }
     }
 };
@@ -63,5 +76,11 @@ export default {
         border: 1px solid #485460;
         background: #d2dae2;
         color: #485460;
+    }
+
+    .error {
+        color: #ff3f34;
+        width: 100%;
+        text-align: center;
     }
 </style>
